a628d5785b36d76d3db84e25f2a74fc62423d4f0
Priya Balasubramanian <priya.balasubramanian7@gmail.com>
2019-07-04 20:58:51 -0700

    Update memoization code

--- memoization_project/lib/leet_code_518.js
+++ memoization_project/lib/leet_code_518.js
@@ -1,3 +1,29 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+var change = function (amount, coins) {
+
+    if (amount === 0) return 0;
+
+    let numCoins = [];
+    coins.forEach(coin => {
+        if( coin <= amount) {
+            numCoins.push(change(amount - coin, coins) + 1);
+        }
+    })
+
+    return numCoins.length;
+
+};
+
+let amount = 5, coins = [1, 2, 5];
+
+console.log(change(amount, coins));
+
+amount = 3, coins = [2]
+
+console.log(change(amount, coins));
+
+amount = 10, coins = [10]
+
+console.log(change(amount, coins));
